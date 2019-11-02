191a8e165098e5e4b1f0b0d2e2078480dfdd434f
Noah Colangelo <noah.colangelo@onteriotechu.net>
2019-11-02 16:21:22 -0400

    added in base script for crouching, and added in the focusing mechanic( a very basic version of it that can be improved upon

---
+++ Assets/GreyBoxing/GBscripts/focusing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class focusing : MonoBehaviour
+{
+    public Camera camera;
+    public Vector3 cameraPosition;
+    public Vector3 cameraAngle;
+    private bool activate = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activate && Input.GetKey(KeyCode.E))
+        {
+            camera.GetComponent<cameraMovement>().enabled = false;
+            camera.transform.position = cameraPosition;
+            camera.transform.rotation = Quaternion.Euler(cameraAngle);
+        }
+        else
+            camera.GetComponent<cameraMovement>().enabled = true;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Sister")
+            activate = true;
+        else
+            activate = false;
+    }
+}
