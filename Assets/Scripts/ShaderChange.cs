d48946d9f5cf866f036f3e22a1d956dde234460d
Kevin <kevin.lounsbury@uoit.net>
2019-11-26 08:56:31 -0500

    Making things work, changing material shaders

---
+++ Assets/Scripts/ShaderChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderChange : MonoBehaviour
+{
+    //Shader normalShader;
+    //MeshRenderer meshRenderer;
+    void Start()
+    {
+       //normalShader = Shader.Find("HDRP/Lit");
+       //
+       //meshRenderer = GetComponent<MeshRenderer>();
+       //meshRenderer.material.shader = normalShader;
+    }
+
+}
