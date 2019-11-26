0aa75321ebd7ed283fbb260be67df8980cc33e12
NoahColangelo <noah.colangelo@uoit.net>
2019-11-26 00:03:51 -0500

    adds lerp to half of the focusing mechanic and adds another tree in for prefabs

--- Assets/GreyBoxing/GBscripts/focusing.cs
+++ Assets/GreyBoxing/GBscripts/focusing.cs
@@ -5,32 +5,40 @@ using UnityEngine;
 public class focusing : MonoBehaviour
 {
     public Camera camera;
+    private Vector3 currentCameraPos;
+    private Quaternion currentCameraAngle;
     public Vector3 cameraPosition;
     public Vector3 cameraAngle;
+    private Vector3 velocity = Vector3.zero;
     private bool activate = false;
 
     // Update is called once per frame
     void Update()
     {
-        if(activate && Input.GetKey(KeyCode.E))
+        currentCameraPos = camera.transform.position;
+        currentCameraAngle = camera.transform.rotation;
+
+        if (activate && Input.GetKey(KeyCode.E))
         {
             camera.GetComponent<cameraMovement>().enabled = false;
-            camera.transform.position = cameraPosition;
-            camera.transform.rotation = Quaternion.Euler(cameraAngle);
+            camera.transform.position = Vector3.SmoothDamp(currentCameraPos, cameraPosition, ref velocity, 0.75f);
+            camera.transform.rotation = Quaternion.Slerp(currentCameraAngle, Quaternion.Euler(cameraAngle), 0.05f);
         }
         else
             camera.GetComponent<cameraMovement>().enabled = true;
+        
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Sister")
+        if (collision.gameObject.tag == "Sister" || collision.gameObject.tag == "Brother")
             activate = true;
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.tag == "Sister")
+        if (collision.gameObject.tag == "Sister" || collision.gameObject.tag == "Brother")
             activate = false;
     }
 }
