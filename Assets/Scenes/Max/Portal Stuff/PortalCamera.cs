0d5381ea2810c3b1d56148d8aaba6212a2b4d609
Max <max.kemp@ontariotechu.net>
2019-11-28 22:49:44 -0500

    Improved portal shader graph effect.

--- Assets/Scenes/Max/Portal Stuff/PortalCamera.cs
+++ Assets/Scenes/Max/Portal Stuff/PortalCamera.cs
@@ -38,9 +38,9 @@ public class PortalCamera : MonoBehaviour
 
 
 
-        Matrix4x4 p = _originalPerspective;
-        p.m01 += Mathf.Sin(Time.time * 1.2F) * 0.1F;
-        p.m10 += Mathf.Sin(Time.time * 1.5F) * 0.1F;
-        _portalCam.projectionMatrix = p;
+        //Matrix4x4 p = _originalPerspective;
+        //p.m01 += Mathf.Sin(Time.time * 1.2F) * 0.1F;
+        //p.m10 += Mathf.Sin(Time.time * 1.5F) * 0.1F;
+        //_portalCam.projectionMatrix = p;
     }
 }
