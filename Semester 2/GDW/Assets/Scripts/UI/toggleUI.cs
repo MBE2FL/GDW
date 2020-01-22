2acba46c2434ac7316b19582579af79e746f6357
NoahColangelo <noah.colangelo@uoit.net>
2020-01-21 23:33:56 -0500

    added in ui to NEW test area scene, press tab to toggle, very basic atm but it does aminate

---
+++ Semester 2/GDW/Assets/Scripts/UI/toggleUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool activeUI;
+    private bool showUI = false;
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        activeUI = animator.GetBool("toggled");
+        Debug.Log(activeUI);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && showUI)
+        {
+            showUI = false;
+            animator.SetBool("toggled", false);
+        }
+        else if(Input.GetKeyDown(KeyCode.Tab) && !showUI)
+        {
+            showUI = true;
+            animator.SetBool("toggled", true);
+        }
+
+    }
+}
