283ea6071faa2c6bd69a1ad31d7ffe0c66d5643b
NoahColangelo <noah.colangelo@uoit.net>
2020-02-05 16:19:57 -0500

    Added in a pop up feature for picking up items to my test scene

---
+++ Semester 2/GDW/Assets/Scripts/Movement/InteractPopUp.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractPopUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool inInteractRange = false;
+    public GameObject popUp;
+    public Transform camPos;
+    private Vector3 popUpPos = new Vector3(0, -100, 0);
+    
+    void Start()
+    {
+        popUp.transform.position = popUpPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inInteractRange)
+        {
+            popUp.transform.position = transform.position + new Vector3(0, 2, 0);
+            popUp.transform.rotation = Quaternion.LookRotation(-camPos.transform.up);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Brother" || collision.transform.tag == "Sister")
+        {
+            inInteractRange = true;
+            popUp.transform.position = transform.position + new Vector3(0, 2, 0);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(inInteractRange)
+        {
+
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.tag == "Brother" || collision.transform.tag == "Sister")
+        {
+            inInteractRange = false;
+            popUp.transform.position = popUpPos;
+        }
+    }
+}
