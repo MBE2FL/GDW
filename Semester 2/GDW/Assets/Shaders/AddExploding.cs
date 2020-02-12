e9ad8adddaae9cba47a8466e378ac56c440a22e8
SnugSiamang55 <kevin.lounsbury@uoit.net>
2020-02-11 22:16:19 -0500

    Crazy good optimization for mesh exploder for realtime

--- Semester 2/GDW/Assets/Shaders/AddExploding.cs
+++ Semester 2/GDW/Assets/Shaders/AddExploding.cs
@@ -11,7 +11,15 @@ public class AddExploding : MonoBehaviour
         for (int i = 0; i < meshList.Length; i++)
         {
             if (meshList[i].gameObject.GetComponent<ExplodingMesh>() == null && meshList[i].sharedMesh.isReadable)
+            {
                 meshList[i].gameObject.AddComponent(typeof(ExplodingMesh));
+                if (meshList[i].gameObject.GetComponent<MeshRenderer>().material != null)
+                {
+                    Texture tex = meshList[i].gameObject.GetComponent<MeshRenderer>().material.mainTexture;
+                    meshList[i].gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Shader Graphs/GeoGraph");
+                    meshList[i].gameObject.GetComponent<MeshRenderer>().material.SetTexture("BIG atlas", tex);
+                }
+            }
            
         }
     }
