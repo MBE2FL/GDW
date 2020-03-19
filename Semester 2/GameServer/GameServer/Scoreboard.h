c5b5db185b9c40e2d7460e2004ae3fb560754b2a
SnugSiamang55 <kevin.lounsbury@uoit.net>
2020-03-18 20:54:01 -0400

    Scoreboard Reading, Writing, and Sorting should be done, needs to be tested.

---
+++ Semester 2/GameServer/GameServer/Scoreboard.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Scoreboard
+{
+public:
+	Scoreboard();
+	~Scoreboard();
+
+	void Read();
+	void Write();
+
+	void Sort();
+
+
+private:
+	struct time
+	{
+		int minutes;
+		float seconds;
+	};
+
+	struct playerTime
+	{
+		std::string teamName;
+		time totalTime;
+	};
+	
+	bool CompareTime(playerTime t1, playerTime t2);
+	
+	std::vector<playerTime> playerTimes;
+};
+
