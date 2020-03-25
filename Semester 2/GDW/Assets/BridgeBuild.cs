f38348a3641c0ed4c8a114732a0615687044fb0d
SnugSiamang55 <kevin.lounsbury@uoit.net>
2020-03-25 12:20:03 -0400

    Bridge now builds itself at correct time

---
+++ Semester 2/GDW/Assets/BridgeBuild.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeBuild : MonoBehaviour
+{
+    bool build = false;
+    void Update()
+    {
+        if(!build)
+            GetComponent<ExplodingMesh>().lerp = -0.6f;
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Sister" || other.transform.tag == "Brother")
+        {
+            build = true;
+        }
+    }
+}
